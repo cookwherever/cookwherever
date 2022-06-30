4b9a392a566fc04b4abb4bce46ced4119a5b46c3
breadchris <chris@lunasec.io>
2022-06-30 14:51:09 -0700

    ignore graph update schema

--- hasura/migrations/1655689995306_add_ingredient_selection_table/up.sql
+++ hasura/migrations/1655689995306_add_ingredient_selection_table/up.sql
@@ -1,47 +1,4 @@
-
-DROP VIEW "public"."most_common_ingredients";
-
-CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS 
- SELECT recipe_ing.id, recipe_ing.name, ing.ingredient_count FROM (
-   SELECT
-   name, count(*) AS ingredient_count
-   FROM recipe_ingredients
-  GROUP BY name
-  ORDER BY (count(*)) DESC
-  ) ing JOIN recipe_ingredients recipe_ing ON recipe_ing.name = ing.name;
-
-DROP VIEW "public"."most_common_ingredients";
-
-CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS 
- SELECT recipe_ing.id,
-    recipe_ing.name,
-    ing.ingredient_count
-    FROM (
-        SELECT recipe_ingredients.name, count(*) AS ingredient_count
-        FROM recipe_ingredients
-        GROUP BY recipe_ingredients.name
-    ) ing
-    JOIN recipe_ingredients recipe_ing ON recipe_ing.name = ing.name
-    ORDER BY ing.ingredient_count DESC;
-
-CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS 
-    SELECT
-    id, name, count(*) AS ingredient_count
-    FROM recipe_ingredients
-    GROUP BY name, id
-    ORDER BY ingredient_count DESC;
-
-CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS 
- SELECT recipe_ingredients.id,
-    recipe_ingredients.name,
-    count(*) AS ingredient_count
-   FROM recipe_ingredients
-  GROUP BY recipe_ingredients.name, recipe_ingredients.id
-  ORDER BY ingredient_count DESC;
-
-DROP VIEW "public"."most_common_ingredients";
-
-CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS 
+CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS
  SELECT
     recipe_ingredients.name,
     count(*) AS ingredient_count
@@ -54,49 +11,15 @@ alter table "public"."ingredients" drop column "names" cascade;
 CREATE TABLE "public"."ingredient_names" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "name" text NOT NULL, "ingredient_id" uuid NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("ingredient_id") REFERENCES "public"."ingredients"("id") ON UPDATE cascade ON DELETE cascade);COMMENT ON TABLE "public"."ingredient_names" IS E'Names of ingredients';
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE OR REPLACE FUNCTION public.food_candidates_for_ingredient(search recipe_ingredients)
- RETURNS SETOF food
- LANGUAGE sql
- STABLE
-AS $function$
-SELECT *
-FROM search_foods(search.name);
-
-$function$;
-
-CREATE OR REPLACE FUNCTION public.food_candidates_for_recipe_ingredient(search recipe_ingredients)
- RETURNS SETOF food
- LANGUAGE sql
- STABLE
-AS $function$
-SELECT *
-FROM search_foods(search.name);
-
-$function$;
-
-alter table "public"."ingredients" add column "name" text
- null;
+alter table "public"."ingredients" add column "name" text;
 
 CREATE OR REPLACE FUNCTION public.food_candidates_for_ingredient(search ingredients)
  RETURNS SETOF food
  LANGUAGE sql
  STABLE
 AS $function$
-SELECT *
-FROM search_foods(search.name);
-
+SELECT * FROM search_foods(search.name);
 $function$;
-
-CREATE OR REPLACE FUNCTION public.food_candidates_for_ingredient(search ingredients)
- RETURNS SETOF food
- LANGUAGE sql
- STABLE
-AS $function$
-SELECT *
-FROM search_foods(search.name);
-
-$function$;
-
 
 alter table "public"."recipe_ingredient_food_candidate" drop constraint "recipe_ingredient_food_candidate_recipe_ingredient_id_fkey";
 
@@ -140,15 +63,7 @@ alter table "public"."ingredient_food_candidate" drop column "ingredient_id" cas
 
 alter table "public"."ingredient_food_candidate" add constraint "ingredient_food_candidate_food_candidate_id_food_candidate_portion_id_key" unique ("food_candidate_id", "food_candidate_portion_id");
 
-CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS 
- SELECT recipe_ingredients.name,
-    count(*) AS ingredient_count,
-    (SELECT id FROM ingredients WHERE name = recipe_ingredients.name)
-   FROM recipe_ingredients
-  GROUP BY recipe_ingredients.name
-  ORDER BY (count(*)) DESC;
-
-CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS 
+CREATE OR REPLACE VIEW "public"."most_common_ingredients" AS
  SELECT recipe_ingredients.name,
     count(*) AS ingredient_count,
     (SELECT id FROM ingredients WHERE name = recipe_ingredients.name)
@@ -163,7 +78,5 @@ CREATE OR REPLACE FUNCTION public.ingredient_for_recipe_ingredient(search recipe
  LANGUAGE sql
  STABLE
 AS $function$
-SELECT *
-FROM ingredients WHERE name = search.name;
-
+SELECT * FROM ingredients AS ing WHERE ing.name = search.name;
 $function$;
